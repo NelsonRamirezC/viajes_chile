16d2c128db57cb90df421067bffe6c1aaa0df3ac
Nelson <xhyskoax@gmail.com>
2022-07-11 23:01:38 -0400

    sección x creada

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(function () {
+
+    //esto hace funcionar los tooltips
+    $('[data-toggle="tooltip"]').tooltip();
+
+    //esto hace funcionar los popover
+    $('[data-toggle="popover"]').popover();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
